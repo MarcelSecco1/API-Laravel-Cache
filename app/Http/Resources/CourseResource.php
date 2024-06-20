b6af69ca90021d5a53f5552bc2154e929db4ce2e
MarcelSecco1 <marcelsecco1@gmail.com>
2024-06-20 15:18:18 -0300

    feat: add resouce to all datas

--- app/Http/Resources/CourseResource.php
+++ app/Http/Resources/CourseResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,11 @@ class CourseResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'identify' => $this->uuid,
+            'name' => $this->name,
+            'description' => $this->description,
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
+        ];
     }
 }
