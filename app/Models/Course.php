5412f458b89f7d5631ccb0bcb3b6a2e6fc7e75a8
MarcelSecco1 <marcelsecco1@gmail.com>
2024-06-20 20:58:57 -0300

    feat: relationship with the Course and Module

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,4 +10,12 @@ class Course extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description'];
+
+    /* Define the relationship with the Module model
+    *  Um curso tem vários módulos
+    */
+    public function modules()
+    {
+        return $this->hasMany(Module::class);
+    }
 }
