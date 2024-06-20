e233c5a5ab775d3952c6855214ccf7435b326811
MarcelSecco1 <marcelsecco1@gmail.com>
2024-06-20 20:59:24 -0300

    feat: relationship with the Course and Module

---
+++ app/Models/Module.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Module extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['name'];
+
+    /* Define the relationship with the Course model
+    *  Um módulo pertence a vários cursos
+    */
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+}
